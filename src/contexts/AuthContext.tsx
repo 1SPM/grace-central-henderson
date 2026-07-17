c8e51dac4fcb067dacfc0ba44429e87473502cfa
1SPM <40576139+1SPM@users.noreply.github.com>
2026-07-17 14:56:12 -0700

    Change SignIn routing from 'path' to 'virtual'

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -481,7 +481,7 @@ export function SignInPage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
-      <SignIn routing="path" path="/sign-in" />
+      <SignIn routing="virtual" />
     </div>
   );
 }
